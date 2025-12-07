c2368469ec9a797c6273d73cb6e853288f48fc3a
alok <alok9601377611@gmail.com>
2025-12-08 00:56:43 +0530

    sqlite issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,30 +1,23 @@
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: [
-    '@nuxt/image',
-    '@nuxt/content',
-    '@nuxt/ui',
-    '@nuxtjs/color-mode'
-  ],
+  modules: ["@nuxt/image", "@nuxt/content", "@nuxt/ui", "@nuxtjs/color-mode"],
   components: {
-    dirs: [
-      {path: '~/components', global: true}
-    ]
+    dirs: [{ path: "~/components", global: true }],
   },
-  css: [
-    '~/assets/css/main.css',
-    '~/assets/css/style.css'
-  ],
+  css: ["~/assets/css/main.css", "~/assets/css/style.css"],
   colorMode: {
-    preference: 'system',
-    fallback: 'light',
-    classSuffix: ''
+    preference: "system",
+    fallback: "light",
+    classSuffix: "",
   },
   nitro: {
+    rollupConfig: {
+      external: ["sqlite3", "better-sqlite3"],
+    },
     prerender: {
       crawlLinks: true,
-      routes: ['/sitemap.xml', '/rss.xml']
-    }
-  }
-})
+      routes: ["/sitemap.xml", "/rss.xml"],
+    },
+  },
+});
